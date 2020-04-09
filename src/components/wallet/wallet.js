22c7a57aeaf2d6570d3d42329d40952a68ea84c0
adwulfran <adrienwulfran@gmail.com>
2020-04-09 21:51:43 +0200

    Add modal to access wallet with mnemonic

--- src/components/wallet/wallet.js
+++ src/components/wallet/wallet.js
@@ -3,9 +3,32 @@ import { Arianee } from "@arianee/arianeejs";
 import "./wallet.css";
 import { Card, Col, Row } from "antd";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-export default class WalletComponent extends React.Component {
-  
+// modal component to access wallet
+import { Modal, Button } from "antd";
+import MnemonicModal from "./modal/mnemonic-modal";
 
+export default class WalletComponent extends React.Component {
+  state = { visible: false };
+  
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
   render() {
     
     return (
@@ -13,17 +36,25 @@ export default class WalletComponent extends React.Component {
         <h2> Access My Wallet {/*this.state.All*/}</h2>
         <div>
           Do not have a wallet?{" "}
-          
-            {" "}
-           
-            <Link to="/create-wallet" >Create A New Wallet</Link>
-          
+          <Link to="/create-wallet">Create A New Wallet</Link>
         </div>
         <div className="site-card-wrapper">
           <Row gutter={24}>
             <Col span={12}>
               <Card title="Mnemonic" bordered={true}>
-                Access wallet with Mnemonic
+                <div>
+                  <Button type="primary" onClick={this.showModal}>
+                    Access by Mnemonic Phrase
+                  </Button>
+                  <Modal
+                    title="Access by Mnemonic Phrase"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                  >
+                    <MnemonicModal></MnemonicModal>
+                  </Modal>
+                </div>
               </Card>
             </Col>
             <Col span={12}>
