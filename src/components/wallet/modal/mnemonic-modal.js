d6a21c69675ed5a8116f7aa818a6e60be3948897
adwulfran <adrienwulfran@gmail.com>
2020-04-12 00:43:10 +0200

    Verifying mnemonic filled

--- src/components/wallet/modal/mnemonic-modal.js
+++ src/components/wallet/modal/mnemonic-modal.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom'
-import { Form, Row, Col, Input, Button } from 'antd';
-import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Form, Row, Col, Input, Button } from "antd";
+import { DownOutlined, UpOutlined } from "@ant-design/icons";
+import { Arianee } from "@arianee/arianeejs";
+import "./mnemonic-modal.css";
 
 const MnemonicModal = () => {
-  const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(true);
   const [form] = Form.useForm();
 
   const getFields = () => {
-    const count = expand ? 10 : 6;
+    //const count = expand ? 10 : 6;
+    const count = 12;
     const children = [];
 
     for (let i = 0; i < count; i++) {
@@ -16,25 +19,49 @@ const MnemonicModal = () => {
         <Col span={8} key={i}>
           <Form.Item
             name={`field-${i}`}
-            label={`Field ${i}`}
+            label={`${i + 1}`}
             rules={[
               {
-                required: true,
-                message: 'Input something!',
+                required: false,
+                message: "Input something!",
               },
             ]}
           >
             <Input placeholder="placeholder" />
           </Form.Item>
-        </Col>,
+        </Col>
       );
     }
 
     return children;
   };
 
-  const onFinish = values => {
-    console.log('Received values of form: ', values);
+  const onFinish = (values) => {
+    function createVariables() {
+      var mnemonicInput = "";
+      for (var i = 0; i < 12; ++i) {
+        mnemonicInput = mnemonicInput + values["field-" + i] + " ";
+      }
+      //console.log('fuckb '+mnemonicInput.length)
+      var k = Math.abs(mnemonicInput.length-1);
+      var l = mnemonicInput.length;
+      console.log(mnemonicInput.slice(0,k))
+      return (mnemonicInput.slice(0,k))
+    }
+    console.log(createVariables());
+
+    const $wallet = new Arianee()
+      .init()
+      .then((arianee) => arianee.fromMnemonic(createVariables()));
+
+    $wallet.then(async (w) => {
+      const b = await w.publicKey;
+      console.log("b is " + b);
+      //this.setState({
+      //  mnemonic: w.mnemnonic,
+      // private_key : w.privateKey
+      //});
+    });
   };
 
   return (
@@ -49,22 +76,13 @@ const MnemonicModal = () => {
         <Col
           span={24}
           style={{
-            textAlign: 'right',
+            textAlign: "right",
           }}
         >
           <Button type="primary" htmlType="submit">
-            Search
+            CONTINUE
           </Button>
-          <Button
-            style={{
-              margin: '0 8px',
-            }}
-            onClick={() => {
-              form.resetFields();
-            }}
-          >
-            Clear
-          </Button>
+
           <a
             style={{
               fontSize: 12,
@@ -81,11 +99,11 @@ const MnemonicModal = () => {
   );
 };
 
-export default MnemonicModal
+export default MnemonicModal;
 ReactDOM.render(
   <div>
     <MnemonicModal />
     <div className="search-result-list">Search Result List</div>
   </div>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
