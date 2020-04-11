3492e252bc9367143723840cfe50353199a4aa56
adwulfran <adrienwulfran@gmail.com>
2020-04-12 00:09:34 +0200

    remove cancel button and ok button from modal footer

--- src/components/wallet/wallet.js
+++ src/components/wallet/wallet.js
@@ -9,28 +9,14 @@ import MnemonicModal from "./modal/mnemonic-modal";
 
 export default class WalletComponent extends React.Component {
   state = { visible: false };
-  
+
   showModal = () => {
     this.setState({
       visible: true,
     });
   };
 
-  handleOk = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  handleCancel = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
   render() {
-    
     return (
       <div>
         <h2> Access My Wallet {/*this.state.All*/}</h2>
@@ -49,8 +35,8 @@ export default class WalletComponent extends React.Component {
                   <Modal
                     title="Access by Mnemonic Phrase"
                     visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
+                    cancelButtonProps={{ style: { display: "none" } }}
+                    okButtonProps={{ style: { display: "none" } }}
                   >
                     <MnemonicModal></MnemonicModal>
                   </Modal>
