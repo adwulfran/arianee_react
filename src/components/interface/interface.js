b1b971c983b1164eb77af4dadcba8a3e659d0394
adwulfran <adrienwulfran@gmail.com>
2020-04-18 13:13:56 +0200

    get url params with useParams() to get privateKey

--- src/components/interface/interface.js
+++ src/components/interface/interface.js
@@ -1,8 +1,22 @@
 import React from "react";
-
+import {
+  useParams
+} from "react-router-dom";
 
 export default class InterfaceComponent extends React.Component {
  render() {
- return ( <div> helo interface</div>)
+ return ( <div> <Interface/></div>)
  }
+}
+
+function Interface() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
 }
